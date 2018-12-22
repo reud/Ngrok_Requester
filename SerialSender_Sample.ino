2092bc88874f8102e206816c2fee44f3e636555f
reiji <31179220+reud@users.noreply.github.com>
2018-12-22 12:16:16 +0900

    Update SerialSender_Sample.ino

--- SerialSender_Sample.ino
+++ SerialSender_Sample.ino
@@ -3,7 +3,7 @@ const int buttonPin=12;
 
  //シリアルで文字さえ送れば検知されるため送る文章(=WORD)はなんでも良いです
 
-const String word="WORD"
+const String words="WORD";
 
 void setup(){
     Serial.begin(9600);
@@ -13,7 +13,7 @@ void setup(){
 void loop(){
     int buttonState=digitalRead(buttonPin);
     if(buttonState==LOW){
-        Serial.prinln(WORD);
+        Serial.println(words);
         delay(200);
     }
 }
